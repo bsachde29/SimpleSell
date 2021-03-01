7f9a76bfc99df981fdb31b07cc997b64f912d36a
Mihir-cs <abhyankm@purdue.edu>
2021-02-28 19:53:22 -0500

    pwcheck commit

---
+++ src/main/java/pwcheck.java
@@ -0,0 +1,15 @@
+import java.util.regex.*;
+public class pwcheck {
+    public static boolean isValidPassword(String password) {
+        String regex = "^(?=.*[0-9])"
+                + "(?=.*[a-z])(?=.*[A-Z])"
+                + "(?=.*[@#$%^&+=])"
+                + "(?=\\S+$).{8,20}$";
+        Pattern p = Pattern.compile(regex);
+        if (password == null) {
+            return false;
+        }
+        Matcher m = p.matcher(password);
+        return m.matches();
+    }
+}
