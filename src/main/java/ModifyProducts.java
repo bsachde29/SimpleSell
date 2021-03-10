3fa84ef62e5224c64e8b4357263980b0bd4fb873
Mihir-cs <abhyankm@purdue.edu>
2021-03-10 00:56:27 -0500

    Modify Products, and changed swunumber to productid in product

--- src/main/java/ModifyProducts.java
+++ src/main/java/ModifyProducts.java
@@ -14,14 +14,76 @@ import java.sql.Statement;
 public class ModifyProducts extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int productID = Integer.parseInt(request.getParameter("ProductID"));
+        String dburl = "jdbc:mysql://selldb.cqt5tgj7qyws.us-east-2.rds.amazonaws.com:3306/simpledb";
+        String dbusername = "simpledb";
+        String dbpassword = "sell1234";
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(dburl, dbusername, dbpassword);
+            if (con != null) {
+                Statement s1 = con.createStatement();
+                System.out.println("Database connection is successful !!!!");
+                String checkProduct = "SELECT * FROM Product WHERE ProductID = '" + productID + "' ";
+                //String checkProductSubCategory = "SELECT * FROM Product_Subcategories WHERE ProductID = '" + productID + "' ";
+                ResultSet productCheck = s1.executeQuery(checkProduct);
+                //ResultSet productCheck1 = s1.executeQuery(checkProductSubCategory);
+                inStock stock = inStock.OUT_OF_STOCK;
+                if (productCheck.getBoolean("inStock")) {
+                    stock = inStock.IN_STOCK;
+                }
+                if (productCheck.next()) {
+                    Product product = new Product(productCheck.getString("Name"),
+                            productCheck.getString("Description"),
+                            productCheck.getString("Category"),
+                            productCheck.getBoolean("hasSubcategories"),
+                            productCheck.getDouble("price") ,
+                            productID, stock, null);
+                    Gson gson = new Gson();
+                    String jsonProduct = gson.toJson(product);
+                    response.getWriter().write(jsonProduct);
+                } else {
+                    response.getWriter().write("Product does not exist");
+                }
+                // check and return appropriate message to response.getWriter
+
+            }
+        } catch (Exception e) {
+            response.getWriter().write("Product Not Registered");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String ProductID = request.getParameter("ProductID");
         String category = request.getParameter("Category");
         String name = request.getParameter("Name");
-        int hasSubcategories = Integer.parseInt(request.getParameter("Has Subcategories"));
+        String has_Subcategories = request.getParameter("hasSubcategories");
         double price = Double.parseDouble(request.getParameter("Price"));
-        int inStock = Integer.parseInt(request.getParameter("In stock"));
+        String in_Stock = request.getParameter("inStock");
         String description = request.getParameter("Description");
-        int isSubProduct = Integer.parseInt(request.getParameter("Is sub product"));
+        String is_SubProduct = request.getParameter("isSubProduct");
+        boolean hasSubcategories, inStock, isSubProduct;
+        if (has_Subcategories.equalsIgnoreCase("true")) {
+            hasSubcategories = true;
+        }
+        else {
+            hasSubcategories = false;
+        }
+        if (in_Stock.equalsIgnoreCase("true")) {
+            inStock = true;
+        }
+        else {
+            inStock = false;
+        }
+        if (is_SubProduct.equalsIgnoreCase("true")) {
+            isSubProduct = true;
+        }
+        else {
+            isSubProduct = false;
+        }
         String url = "jdbc:mysql://selldb.cqt5tgj7qyws.us-east-2.rds.amazonaws.com:3306/simpledb";
         String username = "simpledb";
         String password = "sell1234";
@@ -40,28 +102,20 @@ public class ModifyProducts extends HttpServlet {
                     System.out.println("Product Found");
                     int productId = result.getInt("ProductID");
                     //String checkCategoryQuery = "SELECT * FROM Product WHERE Category = '" + category + "'";
-                    String updateCategory = "UPDATE Product SET Category=" + category + " WHERE ProductID=" + productId;
-                    String updateName = "UPDATE Product SET Name=" + name + " WHERE ProductID=" + productId;
-                    String updateSubcategories = "UPDATE Product SET hasSubcategories=" + hasSubcategories + " WHERE ProductID=" + productId;
-                    String updatePrice = "UPDATE Product SET price=" + price + " WHERE ProductID=" + productId;
-                    String updateInstock = "UPDATE Product SET inStock=" + inStock + " WHERE ProductID=" + productId;
-                    String updateDescription = "UPDATE Product SET Description=" + description + " WHERE ProductID=" + productId;
-                    String updateIsSubProduct = "UPDATE Product SET isSubProduct=" + isSubProduct + " WHERE ProductID=" + productId;
-                    ResultSet result1 = s1.executeQuery(updateCategory);
-                    ResultSet result2 = s1.executeQuery(updateName);
-                    ResultSet result3 = s1.executeQuery(updateSubcategories);
-                    ResultSet result4 = s1.executeQuery(updatePrice);
-                    ResultSet result5 = s1.executeQuery(updateInstock);
-                    ResultSet result6 = s1.executeQuery(updateDescription);
-                    ResultSet result7 = s1.executeQuery(updateIsSubProduct);
-                    if (result1.next() && result2.next() && result3.next() && result4.next()
-                            && result5.next() && result6.next() && result7.next()) {
-                        System.out.println("Updated successfully");
-                    }
-                    else {
-                        System.out.println("update unsuccessful");
-                        response.getWriter().write("update unsuccessful");
-                    }
+                    //String update = "UPDATE Product SET Category=" + category +  " WHERE ProductID=" + productId;
+                    String update = "UPDATE Product SET Category = '" + category + "', Name = '" + name +
+                            "', hasSubCategories = '" + hasSubcategories + "', price = '" + price + "', inStock = '" + inStock +
+                            "', Description = '" + description + "', isSubProduct = '" + isSubProduct
+                            + "' WHERE ProductID = '" + productId + "')";
+                    //ResultSet result1 = s1.executeQuery(update);
+                    s1.executeUpdate(update);
+//                    if (result1.next()) {
+                    System.out.println("Updated successfully");
+//                    }
+//                    else {
+//                        System.out.println("update unsuccessful");
+//                        response.getWriter().write("update unsuccessful");
+//                    }
                 } else {
                     System.out.println("Wrong Details");
                     response.getWriter().write("Wrong Details");
@@ -71,10 +125,5 @@ public class ModifyProducts extends HttpServlet {
             response.getWriter().write("User Not Registered");
             e.printStackTrace();
         }
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 }
