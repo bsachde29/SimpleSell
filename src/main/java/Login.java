cf8c7c4c0f6e4cfff89b3cd41b1ce41af3a58e1e
Mihir-cs <abhyankm@purdue.edu>
2021-04-13 23:17:39 -0400

    Test login

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -19,7 +19,7 @@ public class Login extends HttpServlet {
         String url = "jdbc:mysql://selldb.cqt5tgj7qyws.us-east-2.rds.amazonaws.com:3306/simpledb";
         String username = "simpledb";
         String password = "sell1234";
-        String hashedPass = SHA256Hash.hash(Password);
+        //String hashedPass = SHA256Hash.hash(Password);
         Connection con = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -27,7 +27,7 @@ public class Login extends HttpServlet {
             if (con != null) {
                 System.out.println("Database connection is successful !!!!");
                 Statement s1 = con.createStatement();
-                String check = "SELECT * FROM Sellers WHERE Email = '" + emailID + "' AND Pswd ='" + hashedPass + "'";
+                String check = "SELECT * FROM Sellers WHERE Email = '" + emailID + "' AND Pswd ='" + Password + "'";
                 ResultSet result = s1.executeQuery(check);
                 if (result.next()) {
 
